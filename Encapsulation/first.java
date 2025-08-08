81113996b1af280c8fa7c1fafdb66fb202447f46
dasjoy7 <dasjoy8709@gmail.com>
2025-08-08 22:46:09 +0600

    OOP in Java journey begin

---
+++ Encapsulation/first.java
@@ -0,0 +1,7 @@
+package Encapsulation;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Encapsulation module!");
+    }
+}
---
+++ Introduction/index.java
@@ -0,0 +1,7 @@
+package Introduction;
+
+public class index {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Introduction module!");
+    }
+}
